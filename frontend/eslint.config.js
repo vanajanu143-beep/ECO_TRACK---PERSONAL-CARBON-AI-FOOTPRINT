cb46a45716bfab040123ca1de07fa450b48ee0ba
vanajanu143-beep <vanajanu143@gmail.com>
2026-07-06 14:30:27 +0530

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
+import reactHooks from 'eslint-plugin-react-hHJKooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
